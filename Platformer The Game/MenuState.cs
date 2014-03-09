e8a718f1046e679d11862e944eb1c5d772bb46de
Corentin Garcia <corenting@gmail.com>
2014-03-09 16:44:22 +0100

    Menu deplacable a la souris Changement taille et style fenetre

--- Platformer The Game/MenuState.cs
+++ Platformer The Game/MenuState.cs
@@ -10,15 +10,23 @@ namespace Platformer_The_Game
     class MenuState : IState
     {
         Game game;
-        private static Font menu_font;
-        List<string> menu_list;
-        int selected_pos;
+        private static Font menuFont;
+        List<string> menuList;
+        List<Text> menuBtns = new List<Text>();
+        Text carretLeft, carretRight;
+
+        const int carretLeftPos = 50;
+        const int carretRightPos = 25;
+
+        int selectedPos;
         EventHandler<KeyEventArgs> KeyPressHandler;
         EventHandler<JoystickButtonEventArgs> JoyPressHandler;
+        EventHandler<MouseButtonEventArgs> MouseClickHandler;
+        EventHandler<MouseMoveEventArgs> MouseMoveHandler;
 
-        Image background_image;
-        Sprite background_sprite;
-        Texture background_texture;
+        Image backgroundImage;
+        Sprite backgroundSprite;
+        Texture backgroundTexture;
 
         public void OnEvent(Settings.Action a)
         {
@@ -28,55 +36,60 @@ namespace Platformer_The_Game
         {
             KeyPressHandler = new EventHandler<KeyEventArgs>(onKeyPressed);
             JoyPressHandler = new EventHandler<JoystickButtonEventArgs>(onKeyPressed);
-            menu_list = new List<string>(menuItems);
-            menu_font = font;
-            background_image = new Image(img);
-            background_texture = new Texture(background_image);
-            //800 x 600
-            //? x ?
-            background_sprite = new Sprite(background_texture);
+            MouseClickHandler = new EventHandler<MouseButtonEventArgs>(onMousePressed);
+            MouseMoveHandler = new EventHandler<MouseMoveEventArgs>(onMouseMoved);
+            menuList = new List<string>(menuItems);
+            menuFont = font;
+            backgroundImage = new Image(img);
+            backgroundTexture = new Texture(backgroundImage);
+            backgroundSprite = new Sprite(backgroundTexture);
         }
 
         public void Initialize(Game game)
         {
             this.game = game;
-            background_sprite.Scale = new Vector2f(game.w.Size.X / background_sprite.GetGlobalBounds().Width, game.w.Size.Y / background_sprite.GetGlobalBounds().Height);
+            backgroundSprite.Scale = new Vector2f(game.w.Size.X / backgroundSprite.GetGlobalBounds().Width, game.w.Size.Y / backgroundSprite.GetGlobalBounds().Height);
             game.w.KeyPressed += KeyPressHandler;
             game.w.JoystickButtonPressed += JoyPressHandler;
+            game.w.MouseButtonPressed += MouseClickHandler;
+            game.w.MouseMoved += MouseMoveHandler;
+            carretLeft = new Text("- ", menuFont);
+            carretRight = new Text(" -", menuFont);
+            for (int i = 0; i < menuList.Count; i++)
+            {
+                Text menuItem = new Text(menuList[i], menuFont);
+                uint itemWidth = (uint)menuItem.GetGlobalBounds().Width;
+                uint itemHeight = menuItem.CharacterSize;
+
+                menuItem.Position = new Vector2f(game.w.Size.X / 2 - itemWidth / 2,
+                    game.w.Size.Y / 2 - menuList.Count * itemHeight / 2 + i * itemHeight);
+
+                menuBtns.Add(menuItem);
+            }
         }
 
         public void Update()
         {
+            Text item = menuBtns[selectedPos];
+            uint itemWidth = (uint)item.GetGlobalBounds().Width;
+            uint itemHeight = item.CharacterSize;
 
+            carretLeft.Position = new Vector2f(game.w.Size.X / 2 - itemWidth / 2 - carretLeftPos,
+                game.w.Size.Y / 2 - menuList.Count * itemHeight / 2 + selectedPos * itemHeight);
+
+            carretRight.Position = new Vector2f(game.w.Size.X / 2 + itemWidth / 2 + carretRightPos,
+                game.w.Size.Y / 2 - menuList.Count * itemHeight / 2 + selectedPos * itemHeight);
         }
 
         public void Draw()
         {
-            game.w.Draw(background_sprite);
-            for (int i = 0; i < menu_list.Count; i++)
+            game.w.Draw(backgroundSprite);
+            foreach (Text t in menuBtns)
             {
-                Text menu_item;
-                menu_item = new Text(menu_list.ElementAt(i), menu_font);
-                uint item_width = (uint)menu_item.GetGlobalBounds().Width;
-                uint item_height = menu_item.CharacterSize;
-
-                menu_item.Position = new Vector2f(game.w.Size.X / 2 - item_width / 2, 
-                    game.w.Size.Y / 2 - menu_list.Count * item_height / 2 + i * item_height);
-
-                game.w.Draw(menu_item);
-
-               if (i == selected_pos)
-                {
-                    Text selected = new Text("- ", menu_font);
-                    selected.Position = new Vector2f(game.w.Size.X / 2 - item_width / 2 - 50,
-                        game.w.Size.Y / 2 - menu_list.Count * item_height / 2 + i * item_height);
-                    Text selected2 = new Text(" -", menu_font);
-                    selected2.Position = new Vector2f(game.w.Size.X / 2 + item_width / 2 + 25,
-                        game.w.Size.Y / 2 - menu_list.Count * item_height / 2 + i * item_height);
-                    game.w.Draw(selected);
-                    game.w.Draw(selected2);
-                }
+                game.w.Draw(t);
             }
+            game.w.Draw(carretLeft);
+            game.w.Draw(carretRight);
         }
 
         public void Uninitialize()
@@ -94,45 +107,79 @@ namespace Platformer_The_Game
             onAction(game.settings.GetAction(typeof(MenuState), args.Button));
         }
 
+        public void onMousePressed(object sender, MouseButtonEventArgs args)
+        {
+            if (args.Button != Mouse.Button.Left)
+            {
+                return;
+            }
+            for (int i = 0; i < menuBtns.Count; i++)
+            {
+                FloatRect realRect = getRealRect(menuBtns[i].GetGlobalBounds());
+                if (realRect.Contains(args.X, args.Y))
+                {
+                    onAction(Settings.Action.Use);
+                }
+            }
+        }
+
+        public void onMouseMoved(object sender, MouseMoveEventArgs args)
+        {
+            for (int i = 0; i < menuBtns.Count; i++)
+            {
+                FloatRect realRect = getRealRect(menuBtns[i].GetGlobalBounds());
+                if (realRect.Contains(args.X, args.Y))
+                {
+                    selectedPos = i;
+                }
+            }
+        }
+
+        private FloatRect getRealRect(FloatRect fr)
+        {
+            return new FloatRect(fr.Left - carretLeftPos, fr.Top,
+                fr.Width + carretLeftPos + carretRightPos, fr.Height);
+        }
+
         public void onAction(Settings.Action action)
         {
             switch (action)
             {
                 case Settings.Action.Up:
-                    selected_pos = selected_pos - 1;
+                    selectedPos = selectedPos - 1;
                     break;
                 case Settings.Action.Down:
-                    selected_pos = selected_pos + 1;
+                    selectedPos = selectedPos + 1;
                     break;
                 case Settings.Action.Use:
                     if (ItemSelected != null)
                     {
-                        ItemSelected(this, new ItemSelectedEventArgs(selected_pos, menu_list));
+                        ItemSelected(this, new ItemSelectedEventArgs(selectedPos, menuList));
                     }
                     break;
             }
 
-            if (selected_pos < 0)
+            if (selectedPos < 0)
             {
-                selected_pos = menu_list.Count - 1;
+                selectedPos = menuList.Count - 1;
             }
-            if (selected_pos >= menu_list.Count)
+            if (selectedPos >= menuList.Count)
             {
-                selected_pos = 0;
+                selectedPos = 0;
             }
 
-            Console.WriteLine(selected_pos);
+            Console.WriteLine(selectedPos);
         }
 
         public class ItemSelectedEventArgs : EventArgs
         {
-            public ItemSelectedEventArgs(int selected_pos, List<string> menu_list)
+            public ItemSelectedEventArgs(int selectedPos, List<string> menuList)
             {
-                this.menu_list = menu_list;
-                this.selected_pos = selected_pos;
+                this.menuList = menuList;
+                this.selectedPos = selectedPos;
             }
-            public List<string> menu_list;
-            public int selected_pos;
+            public List<string> menuList;
+            public int selectedPos;
         }
         public delegate void onItemSelected(object sender, ItemSelectedEventArgs args);
         public event onItemSelected ItemSelected;

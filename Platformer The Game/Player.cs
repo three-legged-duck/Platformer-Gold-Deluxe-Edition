76d02bf8aba790dfebe012aaf35fe68bc438a59a
Corentin Garcia <corenting@gmail.com>
2014-03-11 12:12:12 +0100

    Nouveau sprite perso Fix collisions

--- Platformer The Game/Player.cs
+++ Platformer The Game/Player.cs
@@ -38,14 +38,15 @@ namespace Platformer_The_Game
         {
             this.game = game;
             this.gameState = state;
-
+            
             direction = Facing.Right;
             OnGround = true;
             
             // Visual Appearance
-            image = new Image("character1.bmp");
+            image = new Image("character.png");
             image.CreateMaskFromColor(new Color(0, 255, 0));
             this.sprite = new Sprite(new Texture(image));
+            sprite.TextureRect = new IntRect(0, 0, 90, 94);
 
             // Hitbox
             this.Hitbox = new Hitbox(new List<FloatRect> { sprite.GetLocalBounds() });
@@ -56,34 +57,36 @@ namespace Platformer_The_Game
 
         public void Initialize()
         { }
-
+        int currFrame = 0;
         public void Event(Settings.Action action)
         {
             switch (action)
             {
                 case Settings.Action.Left:
                     moving = true;
-                    if (direction == Facing.Right)
+                    sprite.TextureRect = new IntRect(90 * ((currFrame++ % 6) + 1), 0, -90, 94); 
+/*                    if (direction == Facing.Right)
                     {
                         Debug.WriteLine("Changed : Left");
-                        sprite.TextureRect = new IntRect((int)image.Size.X, 0, (int)-image.Size.X, (int)image.Size.Y);
-                    }
+                        sprite.TextureRect = new IntRect((int)sprite.GetLocalBounds().Width, 0, (int)-sprite.GetLocalBounds().Width, (int)sprite.GetLocalBounds().Height);
+                    }*/
                     direction = Facing.Left;
                     break;
                 case Settings.Action.Right:
                     moving = true;
-                    if (direction == Facing.Left)
+                    sprite.TextureRect = new IntRect(90 * (currFrame++ % 6), 0, 90, 94); 
+                    /*if (direction == Facing.Left)
                     {
                         Debug.WriteLine("Changed : Right");
-                        sprite.TextureRect = new IntRect(0, 0, (int)image.Size.X, (int)image.Size.Y);
-                    }
+                        sprite.TextureRect = new IntRect(0, 0, (int)sprite.GetLocalBounds().Width, (int)sprite.GetLocalBounds().Height);
+                    } */ 
                     direction = Facing.Right;
                     break;
                 case Settings.Action.Jump:
                     if (OnGround)
                     {
                         OnGround = false;
-                        speed.Y = -15;
+                        speed.Y = -18;
                     }
                     break;
 
@@ -99,7 +102,20 @@ namespace Platformer_The_Game
             else if (OnGround && speed.Y != 0) speed.Y = 0;
             _pos.X = Math.Min(Math.Max(_pos.X + speed.X, 0), 800 - sprite.GetLocalBounds().Width);
             _pos.Y = Math.Min(Math.Max(_pos.Y + speed.Y, 0), 600 - sprite.GetLocalBounds().Height);
+            if (_pos.X >= 800 - sprite.GetLocalBounds().Width || _pos.X <= 1) speed.X = 0;
             Hitbox.MoveTo(_pos);
+
+            if (!moving && direction == Facing.Right)
+            {
+                sprite.TextureRect = new IntRect(0, 94, 90, 94);
+                currFrame = 0;
+            }
+            else if (!moving && direction == Facing.Left)
+            {
+                sprite.TextureRect = new IntRect(90, 94, -90, 94);
+                currFrame = 0;
+            }
+
             moving = false;
             OnGround = false;
             sprite.Position = _pos;
@@ -108,6 +124,7 @@ namespace Platformer_The_Game
         public void Draw()
         {
             game.w.Draw(sprite);
+            //game.w.Draw(Hitbox);
         }
 
         public void Uninitialize()
@@ -118,11 +135,22 @@ namespace Platformer_The_Game
 
         public void Collided(Platform platform, FloatRect collision)
         {
-            _pos.Y = collision.Top - sprite.TextureRect.Height;
-            Hitbox.MoveTo(_pos);
-            sprite.Position = _pos;
-            OnGround = true;
-
+            if (platform.Pos.Y + platform.hitbox.effectiveShapes[0].Height > Pos.Y + sprite.GetLocalBounds().Height)
+            {
+                //if (collision.Top - Pos.Y
+                _pos.Y = collision.Top - sprite.TextureRect.Height;
+                Hitbox.MoveTo(_pos);
+                sprite.Position = _pos;
+                speed.Y = 0;
+                OnGround = true;
+            }
+            else
+            {
+                _pos.Y = collision.Top + collision.Height;
+                Hitbox.MoveTo(_pos);
+                sprite.Position = _pos;
+                speed.Y = 0;
+            }
         }
     }
 }

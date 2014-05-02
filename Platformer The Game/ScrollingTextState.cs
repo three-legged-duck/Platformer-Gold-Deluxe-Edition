c3a3a868d1b6ae8456868a456c07ca7db3385efe
Corentin Garcia <corenting@gmail.com>
2014-05-02 18:09:31 +0200

    Fix texte qui dépasse dans l'intro

--- Platformer The Game/ScrollingTextState.cs
+++ Platformer The Game/ScrollingTextState.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using SFML.Graphics;
 using SFML.Window;
 
@@ -10,13 +7,13 @@ namespace Platformer_The_Game
 {
     class ScrollingTextState : IState
     {
-        private readonly IState nextState;
-        private Text[] textLines;
+        private readonly IState _nextState;
+        private Text[] _textLines;
         private string textName;
-        private uint textSize;
-        private Game game;
+        private uint _textSize;
+        private Game _game;
 
-        private EventHandler<MouseButtonEventArgs> MouseBtnHandler;
+        private EventHandler<MouseButtonEventArgs> _mouseBtnHandler;
 
         public string BgMusicName
         {
@@ -25,57 +22,58 @@ namespace Platformer_The_Game
 
         public ScrollingTextState(string textName, IState nextState)
         {
-            this.nextState = nextState;
+            _nextState = nextState;
             this.textName = textName;
         }
 
 
         public void Initialize(Game game)
         {
-            textSize = game.W.Size.Y/18;
-            this.game = game;
-
-            MouseBtnHandler = delegate(object sender, MouseButtonEventArgs btn)
+            _textSize = game.W.Size.Y/18;
+            _game = game;
+            _game.W.SetView(_game.W.DefaultView);
+            _mouseBtnHandler = delegate(object sender, MouseButtonEventArgs btn)
             {
                 if (btn.Button == Mouse.Button.Left || btn.Button == Mouse.Button.Right)
                 {
                     EndScrollingText();
                 }
             };
-            game.W.MouseButtonPressed += MouseBtnHandler;
+            game.W.MouseButtonPressed += _mouseBtnHandler;
 
             string[] textFile = File.ReadAllLines(@"res\strings\" + game.Settings.Language + textName + ".txt");
-            textLines = new Text[textFile.Length];
-            for (int i = 0; i < textLines.Length; i++)
+            _textLines = new Text[textFile.Length];
+            for (int i = 0; i < _textLines.Length; i++)
             {
-                textLines[i] = new Text(textFile[i],game.MenuFont,textSize);
-                textLines[i].Position = new Vector2f(0, game.W.Size.Y + ( textSize *i));
+                _textLines[i] = new Text(textFile[i],game.MenuFont,_textSize);
+                _textLines[i].Position = new Vector2f(0, game.W.Size.Y + ( _textSize *i));
             }
         }
 
         public void Update()
         {
-            if (textLines[textLines.Length - 1].Position.Y + textSize < 0)
+            if (_textLines[_textLines.Length - 1].Position.Y + _textSize < 0)
             {
                 EndScrollingText();
             }
-            foreach (Text line in textLines)
+            foreach (Text line in _textLines)
             {
-                line.Position = new Vector2f(0, line.Position.Y - (game.W.Size.Y / 400));
+                // ReSharper disable once PossibleLossOfFraction
+                line.Position = new Vector2f(0, line.Position.Y - (_game.W.Size.Y / 400));
             }
         }
 
         public void Draw()
         {
-            foreach (Text line in textLines)
+            foreach (Text line in _textLines)
             {
-                game.W.Draw(line);
+                _game.W.Draw(line);
             }
         }
 
         public void Uninitialize()
         {
-            game.W.MouseButtonPressed -= MouseBtnHandler;
+            _game.W.MouseButtonPressed -= _mouseBtnHandler;
         }
 
         public void OnEvent(Settings.Action a)
@@ -85,8 +83,8 @@ namespace Platformer_The_Game
 
         private void EndScrollingText()
         {
-            game.State = nextState;
-            game.StopInput(600);
+            _game.State = _nextState;
+            _game.StopInput(600);
         }
     }
 }
